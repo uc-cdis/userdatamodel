52671444c55e4ba033220448bbde5dde08232dee
philloooo <phillis.tt@gmail.com>
2016-09-27 16:04:20 -0500

    fix(models): update provider import

--- userdatamodel/models.py
+++ userdatamodel/models.py
@@ -1,6 +1,6 @@
 from user import (
     User, Project, ResearchGroup, UserAccess,
     IdentityProvider, Application, Certificate,
-    StorageProvider, StorageAccess, Bucket,
-    ComputeProvider, ComputeAccess, UserToBucket,
+    CloudProvider, StorageAccess, Bucket,
+    ComputeAccess, UserToBucket,
     AuthorizationProvider, S3Credential)
