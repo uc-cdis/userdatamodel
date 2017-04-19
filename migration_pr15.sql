12f7c1ebbca97b0f8324fc9274c8bf41e377535b
Thanh Nguyen <thanh.nguyen@uclouvain.be>
2017-04-19 15:35:50 -0500

    fix(migration): add constraints to migration script

--- migration_pr15.sql
+++ migration_pr15.sql
@@ -1,3 +1,4 @@
+ALTER TABLE research_group ALTER COLUMN name TYPE varchar;
 ALTER TABLE user_access RENAME TO access_privilege;
 ALTER TABLE access_privilege
     ADD COLUMN group_id int;
@@ -9,3 +10,7 @@ ALTER TABLE access_privilege
 
 ALTER TABLE access_privilege
   ADD CONSTRAINT uniq_ap UNIQUE(user_id, group_id, project_id);
+
+CREATE UNIQUE INDEX unique_group_project_id ON access_privilege (group_id, project_id) WHERE (user_id is null);
+CREATE UNIQUE INDEX unique_user_project_id ON access_privilege (user_id, project_id) WHERE (group_id is null);
+CREATE UNIQUE INDEX unique_group_project_id ON access_privilege (user_id, group_id) WHERE (project_id is null);
