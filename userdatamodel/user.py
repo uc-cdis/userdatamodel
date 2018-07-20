9c85bddaf1acfd1a4772220e61574a4897cfedb7
Rudyard Richter <rudyardrichter@uchicago.edu>
2018-07-20 15:31:35 -0500

    feat(rbac): remove roles/resources from policy

--- userdatamodel/user.py
+++ userdatamodel/user.py
@@ -142,36 +142,6 @@ class Policy(Base):
     __tablename__ = 'policy'
 
     id = Column(Text, primary_key=True, unique=True)
-    _role_ids = Column(Text)
-    _resource_paths = Column(Text)
-
-    def __init__(self, **kwargs):
-        """
-        To construct a ``Policy``, Convert the lists of role IDs and resource
-        IDs to string serializations where the values are separated by spaces.
-
-        Each has an associated property to read out the values correctly to a
-        list.
-        """
-        if 'role_ids' in kwargs:
-            role_ids = kwargs.pop('role_ids')
-            if any(' ' in role_id for role_id in role_ids):
-                raise ValueError('role IDs may not contain spaces')
-            kwargs['_role_ids'] = ' '.join(role_ids)
-        if 'resource_paths' in kwargs:
-            resource_paths = kwargs.pop('resource_paths')
-            if any(' ' in path for path in resource_paths):
-                raise ValueError('resource paths may not contain spaces')
-            kwargs['_resource_paths'] = ' '.join(resource_paths)
-        super(Policy, self).__init__(**kwargs)
-
-    @property
-    def role_ids(self):
-        return self._role_ids.split(' ')
-
-    @property
-    def resource_paths(self):
-        return self._resource_paths.split(' ')
 
 
 class GoogleProxyGroup(Base):
